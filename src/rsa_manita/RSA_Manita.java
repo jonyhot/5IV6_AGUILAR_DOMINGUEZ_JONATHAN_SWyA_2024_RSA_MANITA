97136262bb253a7a206ac0fe9f7d587e36881b17
jonyhot <jaguilard2100@alumno.ipn.mx>
2023-11-22 13:41:46 -0600

    acabe

---
+++ src/rsa_manita/RSA_Manita.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rsa_manita;
+
+import INTERFAZ.MENU;
+
+/**
+ *
+ * @author JONATHAN
+ */
+public class RSA_Manita {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MENU menu = new MENU();
+        menu.setVisible(true);
+        menu.setLocationRelativeTo(null);
+    }
+    
+}
